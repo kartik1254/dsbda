0d16742e2370b1ba9a433d4f015199b3b75e85f5
kartik1254 <104548785+kartik1254@users.noreply.github.com>
2023-05-28 23:57:56 +0530

    Update selection__sort.py

--- selection__sort.py
+++ selection__sort.py
@@ -1,14 +1,9 @@
-def selectionSort(array, size):
+def selectionSort(arr):
+    for i in range(len(arr)):
+        min = float('-inf')
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i],arr[j] = arr[j], arr[i]
+    return arr
     
-    for ind in range(size):
-        min_index = ind
-        for j in range(ind + 1, size):
-            if array[j] < array[min_index]:
-                min_index = j
-        (array[ind], array[min_index]) = (array[min_index], array[ind])
- 
-arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
-size = len(arr)
-selectionSort(arr, size)
-print('The array after sorting in Ascending Order by selection sort is:')
-print(arr)
+print(selectionSort([54,55,87,56,21,12]))
